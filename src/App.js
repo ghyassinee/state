c0b98b2b820b704a866fb15a17b6035c28454994
ghyassinee <yassine.ghoul@esprit.tn>
2021-08-17 11:09:18 +0200

    yassine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Timer from './timer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      FullName: "Yassine ghoul",
+      Bio: "23 yo",
+      imgSrc:"/profile.jpg",
+      Profession: "yayaya",
+    };
+  }
+  state = {
+    on: false,
+  }
+
+  render() {
+    let showProfile = () => {
+      this.setState({
+        on: !this.state.on
+
+      })
+    };
+    return (
+      <div className="App">
+        {!this.state.on &&
+          <button onClick={showProfile}>Show</button>
+        }
+        {this.state.on &&
+          <button onClick={showProfile}>hide</button>
+        }
+        {this.state.on && <h2>
+          <Timer></Timer>
+<div className="cordon">
+            <img src={this.state.imgSrc} alt=""/>
+          
+            Fullname : {this.state.FullName} <br/>
+            Profession : {this.state.Bio}<br/>
+            Bio : {this.state.Profession}  <br/><br/>
+          
+            </div>
+            </h2>}
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
